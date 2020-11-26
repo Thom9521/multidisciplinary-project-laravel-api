38415be78cb3f082400ad4b04e4f930f0117160d
Thom9521 <thom9521@edu.easj.dk>
2020-11-26 11:55:20 +0100

    Seeder for the product table

---
+++ database/seeders/ProductTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Deleting existing data in the table
+        Product::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 50; $i++) {
+            Product::create([
+                'name' => $faker->name(),
+                'price' => $faker->randomDigit(),
+                'imagePath' => $faker->imageUrl(),
+                'description' => $faker->text(),
+                'category' => "categoryName"
+            ]);
+        }
+    }
+}
